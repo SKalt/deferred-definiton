df43360213f0a6b0b1ff1a5694ff30043f9a6270
SKalt <kalt.steven@gmail.com>
2018-05-28 10:21:58 -0400

    Keep it simple, stupid: delete the non-working OOP implementation

--- src/class.js
+++
@@ -1,9 +0,0 @@
-import {get, set} from './base.js';
-export class Deferred extends Proxy {
-  constructor(object){
-    const pending = {};
-    const resolve = {};
-    super(object, basicHandler(resolve, pending))
-    Object.assign(this, {resolve, pending});
-  }
-}
